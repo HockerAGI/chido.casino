7dc4944eba51af16754006df81965b46e9153d05
Hocker AGI <contacto.hocker@gmail.com>
2026-01-08 23:32:19 -0800

    Actualizar stripe.ts

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,8 +1,9 @@
 import Stripe from "stripe";
 
 export const stripe = new Stripe(
-  process.env.STRIPE_SECRET_KEY!,
+  process.env.STRIPE_SECRET_KEY as string,
   {
-    apiVersion: "2023-10-16",
+    apiVersion: "2024-06-20",
+    typescript: true
   }
 );
